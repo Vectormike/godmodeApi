8226bbb2d8308eec1ad45fe465fa34a76be1a2cd
Vectormike <victorjonah199@gmail.com>
2022-12-29 13:35:06 +0100

    test: add more test cases for classification controller

--- src/api/classification/classification.controller.spec.ts
+++ src/api/classification/classification.controller.spec.ts
@@ -1,7 +1,10 @@
 import { ClassificationController } from './classification.controller';
 import { ClassificationService } from './classification.service';
 import { EtherIntegrationService } from '../../infrastructure/ether/etherIntegration.service';
-import { Classification } from './dto/classification-dto';
+import {
+  Classification,
+  ClassificationQueryParams,
+} from './dto/classification-dto';
 
 describe('ClassificationController', () => {
   let classificationController: ClassificationController;
@@ -38,6 +41,61 @@ describe('ClassificationController', () => {
 
     it('should throw an error if the address is invalid', async () => {
       const query = { address: 'invalid', network: 'mainnet' };
+
+      jest
+        .spyOn(classificationService, 'classify')
+        .mockRejectedValue(new Error('Invalid address or network'));
+
+      await expect(
+        classificationController.Classify(query),
+      ).rejects.toThrowError('Invalid address or network');
+    });
+
+    it('should throw an error if the network is invalid', async () => {
+      const query = { address: '0xabc', network: 'invalid' };
+
+      jest
+        .spyOn(classificationService, 'classify')
+        .mockRejectedValue(new Error('Invalid address or network'));
+
+      await expect(
+        classificationController.Classify(query),
+      ).rejects.toThrowError('Invalid address or network');
+    });
+
+    it('should throw an error if the address and network are invalid', async () => {
+      const query = { address: 'invalid', network: 'invalid' };
+
+      jest
+        .spyOn(classificationService, 'classify')
+        .mockRejectedValue(new Error('Invalid address or network'));
+
+      await expect(
+        classificationController.Classify(query),
+      ).rejects.toThrowError('Invalid address or network');
+    });
+    it('should throw an error if the address is not a string', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const query: ClassificationQueryParams | any = {
+        address: 123,
+        network: 'mainnet',
+      };
+
+      jest
+        .spyOn(classificationService, 'classify')
+        .mockRejectedValue(new Error('Invalid address or network'));
+
+      await expect(
+        classificationController.Classify(query),
+      ).rejects.toThrowError('Invalid address or network');
+    });
+
+    it('should throw an error if the network is not a string', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const query: ClassificationQueryParams | any = {
+        address: '0xabc',
+        network: 123,
+      };
 
       jest
         .spyOn(classificationService, 'classify')
